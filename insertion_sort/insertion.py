be4553853e4e655b4451651ee484aa6d33fd86b9
jetale <vishwajeet.bhosale@colostate.edu>
2020-07-17 01:20:51 -0600

    added golang implementation of insertion sort and cleaned python code

--- insertion_sort/insertion.py
+++ insertion_sort/insertion.py
@@ -7,11 +7,12 @@ import timeit
 def do_work():
 
 	unsorted_list  = []
+	list_size = input('\n Enter list size :  ')
 
-	for i in range(1,10000):
+	for i in range(1,int(list_size)):
 		unsorted_list.append(random.randint(0, 100))
 
-	print(f'\n Unsorted list ------> {unsorted_list}')
+	#print(f'\n Unsorted list ------> {unsorted_list}')
 
 	get_sorted(unsorted_list)
 
@@ -22,21 +23,20 @@ def get_sorted(uns_list):
 
 	len_uns = len(uns_list)
 
-	for i in range(0,len_uns-1):
+	for i in range(1,len_uns-1):
 
-		j = i + 1
+		j = i 
 
-		if uns_list[j] < uns_list[i]:
+		
+		cp_item = uns_list[j]
 
-			cp_item = uns_list[j]
+		while (j> 0 and cp_item < uns_list[j-1]):
 
-			while (j> 0 and cp_item < uns_list[j-1]):
+			#print(n)
+			uns_list[j] = uns_list[j-1]
+			j= j -1
 
-				#print(n)
-				uns_list[j] = uns_list[j-1]
-				j= j -1
-
-			uns_list[j] = cp_item
+		uns_list[j] = cp_item
 
 	end_time = timeit.default_timer()
 
